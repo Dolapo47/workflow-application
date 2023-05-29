741b23d68babfc930ffcb9995c271d8660ede292
user <user@users-MacBook-Pro.local>
2023-05-29 10:22:36 +0100

    modify generate report to download file on browser

--- src/main/java/com/dolapo/workflowapplication/controller/GenerateReport.java
+++ src/main/java/com/dolapo/workflowapplication/controller/GenerateReport.java
@@ -3,30 +3,41 @@ package com.dolapo.workflowapplication.controller;
 import com.dolapo.workflowapplication.collection.WorkItem;
 import com.dolapo.workflowapplication.repository.WorkItemRepository;
 import com.dolapo.workflowapplication.response.ResponseModel;
-import lombok.AllArgsConstructor;
 import net.sf.jasperreports.engine.*;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
-import org.springframework.http.ResponseEntity;
+import org.springframework.core.io.Resource;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
 
-import java.util.ArrayList;
+import javax.print.attribute.standard.Media;
+import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/report")
-@AllArgsConstructor
 public class GenerateReport {
+
+    @Value("${jasper.report.file.path}")
+    private String filePath;
+
+    @Value("${jasper.report.dest.path}")
+    private String destPath;
 
     private final WorkItemRepository workItemRepository;
 
-    @GetMapping
-    public ResponseEntity<ResponseModel> generateReport(){
-        String filePath = "/Users/user/JaspersoftWorkspace/MyReports/workitem-report.jrxml";
+    public GenerateReport(WorkItemRepository workItemRepository) {
+        this.workItemRepository = workItemRepository;
+    }
 
+    @GetMapping
+    public ResponseEntity<byte[]> generateReport(HttpServletResponse response){
+        byte[] downloadedFile;
+        HttpHeaders headers = new HttpHeaders();
         try{
             List<WorkItem> workItems = workItemRepository.findAll();
             Map<String, Object> parameters = new HashMap<>();
@@ -34,11 +45,16 @@ public class GenerateReport {
             JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(workItems);
             JasperReport report = JasperCompileManager.compileReport(filePath);
             JasperPrint fillReport = JasperFillManager.fillReport(report, parameters, dataSource);
-            JasperExportManager.exportReportToPdfFile(fillReport, "/Users/user/Downloads/firstReport.pdf");
-            System.out.println("Report created...");
+            downloadedFile = JasperExportManager.exportReportToPdf(fillReport);
+            headers.setContentType(MediaType.valueOf("application/pdf"));
+            ContentDisposition build = ContentDisposition
+                    .builder("attachment")
+                    .filename("WorkItems")
+                    .build();
+            headers.setContentDisposition(build);
         }catch(Exception e){
             return null;
         }
-        return null;
+        return new ResponseEntity<>(downloadedFile, headers, HttpStatus.OK);
     }
 }
